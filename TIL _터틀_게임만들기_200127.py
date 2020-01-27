bc6c2584b0db5fc71e2b35a6d18e22b59f20bbce
951237 <951237.gmail.com>
2020-01-28 02:02:50 +0900

    Docs : Update - 게이머 추가

--- TIL _터틀_게임만들기_200127.py
+++ TIL _터틀_게임만들기_200127.py
@@ -5,4 +5,13 @@ wn.title('Making Game')
 wn.bgcolor('green')
 wn.setup(width = 800, height = 600)
 
+# 게이머 추가
+player1 = turtle.Turtle()
+player1.speed(0)
+player1.penup()
+player1.shape("square")
+player1.color('white')
+player1.goto(0, -250)
+
+
 wn.mainloop()
