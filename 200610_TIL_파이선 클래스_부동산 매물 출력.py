db4ede24a1c5d940d1d1e06b097c089670d35323
951237 <951237@gmail.com>
2020-06-10 23:56:00 +0900

    Doc : Update - 예제코드 문제 추가

--- 200610_TIL_파이선 클래스_부동산 매물 출력.py
+++ 200610_TIL_파이선 클래스_부동산 매물 출력.py
@@ -1,3 +1,11 @@
+# 200610_TIL_부동산 프로그램 작성
+
+# 출력예제
+# 총 3대의 매물이 있습니다.
+# 강남 아파트 매매 10억 2010년
+# 마포 오피스텔 전세 5억 2007년
+# 송파 빌라 월세 500/5 2000년
+
 class House:
     # 매물초기화
     def __init__(self, location, house_type, deal_type, price, completion_year):
