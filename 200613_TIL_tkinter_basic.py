f7c284dae12d4bd58d26887cc4bb0f692256a3ca
951237 <951237@gmail.com>
2020-06-13 22:34:07 +0900

    Doc : Update - 주석처리

--- 200613_TIL_tkinter_basic.py
+++ 200613_TIL_tkinter_basic.py
@@ -1,16 +1,16 @@
 from tkinter import *   # 라이브러리 호출
 
-root = Tk()
-root.title("951237 tkinter study")
-root.geometry("640x480")
+root = Tk()     # tkinter 오브젝트 셍성
+root.title("951237 tkinter study")      # 윈도우창 제목 설정
+root.geometry("640x480")        # 윈도우 크기 설정 / 가록 * 세로
 
-def btncmd():
+def btncmd():       # 윈도우 버튼 명령 함수
     pass
 
-btn = Button(root, text = "주문", command=btncmd)
-btn.pack()
+btn = Button(root, text = "주문", command=btncmd)       # 버튼 디자인 및 명령 함수 링크
+btn.pack()      # 버튼 생성
 
-root.mainloop()
+root.mainloop()     # 윈도우창 항상 켜져있도록 
 
 
 
