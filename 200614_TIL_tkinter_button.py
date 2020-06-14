4fc96e580dd9e74ea5636613a9f3a0a9b3684f8a
951237 <951237@gmail.com>
2020-06-15 01:36:38 +0900

    Doc : Update - 버튼을 이미지로 만들기 / 주석달기

--- 200614_TIL_tkinter_button.py
+++ 200614_TIL_tkinter_button.py
@@ -7,11 +7,15 @@ root.geometry("640x480")
 btn1 = Button(root, text="버튼1")
 btn1.pack()
 
-btn2 = Button(root, padx=5, pady = 10, text="버튼2")
+btn2 = Button(root, padx=5, pady = 10, text="버튼2")    # 버튼내에서 x, y 축으로 여백주기
 btn2.pack()
 
 btn3 = Button(root, padx=10, pady=5, text="버튼3")
 btn3.pack()
+
+photo = PhotoImage(file="img.png")      # 버튼으로 사용할 이미지 불러오기
+btn4 = Button(root, image=photo)    # 버튼을 이미지로 저장하기
+btn4.pack()
 
 
 root.mainloop()
