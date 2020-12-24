094c5377dab2efc57a3148cc5d318987d2159d04
951237 <44091528+951237@users.noreply.github.com>
2020-12-24 23:44:24 +0900

    Create 201224_TIL_칼럼의 응답값 빈도수 그래프로 표시하기.py
    - 설문의 문항을 그래프로 표현
    - 활용 : 반복문을 이용할 경우 각 문항의 응답을 그래프로 표현가능함.

---
+++ 201224_TIL_칼럼의 응답값 빈도수 그래프로 표시하기.py
@@ -0,0 +1,15 @@
+'''
+qno : question_no, ex)Q12
+fsize : figsize default (10,6)
+order : optional order list, default value_counts().index
+'''
+def show countplot_by_qno(qno, fsize=(10, 6), order=None)
+	if not order:	# order 값이 없는 경우
+		order = answer[qno].value_counts().index	
+
+	plt.figure(figsize = fsize)
+	sns.countplot(data=answer,
+					y = qno,
+					order = order,
+					palette="Blues_r"
+					).set_title(question[qno])
