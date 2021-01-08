1961f5c1e99d9ef82407be40bc379c3c21249cc4
951237 <951237@gmail.com>
2021-01-08 15:19:55 +0900

    Doc : Create - 대학원 문제 풀이

---
+++ TIL_알고리즘/210106_대학원_문자열 출력.py
@@ -0,0 +1,15 @@
+lst = [
+'반짝 반짝 작은 별',
+'   아름답게 비치네',
+'      서쪽 하늘에서도',
+'      동쪽 하늘에서도',
+]
+
+
+print('-' * 21)
+for i in lst:
+    print(i)
+print('')
+for i in lst[:2]:
+    print(i)
+print('\n', '-' * 21)
