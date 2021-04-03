e7d498526181efa5e05a8bd4b82749d4e374d690
951237 <951237@gmail.com>
2021-04-04 01:57:25 +0900

    Doc : Create - 백준 알고리즘 10809

--- TIL_2021/210402_백준_10809_정답.py
+++ TIL_2021/210402_백준_10809_정답.py
@@ -1,11 +1,6 @@
 # 입력받은 단어에 사용된 알파벳의 사용여부 및 위치 출력하기
-S = input()
-alphabet = "abcdefghijklmnopqrstuqwz"    
-result = []
+word = input()
+alphabet = list(range(97,123))  # 아스키코드 숫자 범위
+
 for i in alphabet:
-    if i in S:  # 알파벳이 입력단어에 들어 있으면
-        k = S.find(i)  # 입력단어에서 글자의 위치를 찾기
-        result.append(str(k))
-    else:
-        result.append('-1')
-print(' '.join(result))  # 리스트를 ' '으로 분리하여 출력하기
+    print(word.find(char(x)))   # char() : 숫자를 문자로 , ord() : 문자를 숫자로
