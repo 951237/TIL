28dc46d9cac903e0bc5913ccdedd7190daa1416f
951237 <951237@gmail.com>
2021-04-07 23:57:46 +0900

    Doc : Create - 나동빈 크롤링 클래스 예제 따라하기

---
+++ TIL_2021/210407_클래스 예제.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+class ConverSation:
+    # 질문, 응답 두변수로 구분
+    def __init__(self, qustion, answer):
+        self.question = question
+        self.answer = answer
+
+    def get_subjects():
+        pass
