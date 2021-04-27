1d2ccb9f31d990ce070ef6544c0c4703cc037d53
951237 <951237@gmail.com>
2021-04-28 00:02:22 +0900

    Doc : Update - 2020년 비가 온 횟수 구하기 - 비의 양을 입력받아서 데이터 값을 반영하여 결과 추출

--- TIL_2021/210426_프로젝트_2020년에는 며칠이나 비가 왔을까.py
+++ TIL_2021/210426_프로젝트_2020년에는 며칠이나 비가 왔을까.py
@@ -7,9 +7,24 @@ import tqdm
     # 파일 경로
 PATH_SRC = 'coding_TIL/TIL_2021/210426_데이터셋_2020년 기온 데이터_강수량 프로젝트.csv'
 
-df = pd.read_csv(PATH_SRC, encoding='cp949')
-print(df)
-# 강수량으로 비가온 날짜 추출
-# 카운트 하기
-# 월별 강수량 그래프 출력
+# 클래스로 리팩토링
+class Weather_2020:
+    def __init__(self, path_src=''):
+        self.path_src = path_src
 
+    def make_df_rain(self, path_src):
+        df = pd.read_csv(path_src, encoding='cp949')
+        # 원하는 칼럼만 추출 df[[,,,]]
+        df = df[['일시', '평균기온(°C)', '최저기온(°C)', '최고기온(°C)',
+                 '일강수량(mm)', '최대 풍속(m/s)', '평균 풍속(m/s)']]
+        return df
+
+    def count_rain_day(self, df):
+        rain_amount = input('얼마이상의 강수량의 날을 알고 싶으세요? ')
+        # 2020년 비가 온날 카운트하기
+        day_rain = df.loc[df['일강수량(mm)'] > int(rain_amount)]
+        print(f'2020년에 {rain_amount}mm이상 비가 온날은 {len(day_rain)}일 입니다.')
+
+weather = Weather_2020()
+df = weather.make_df_rain(PATH_SRC)
+weather.count_rain_day(df)
