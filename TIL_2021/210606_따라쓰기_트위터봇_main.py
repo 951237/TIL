81cfee951f67d88263ea8e55a8ac78776363ff74
951237 <951237@gmail.com>
2021-06-07 00:08:49 +0900

    Doc : Create - 뉴스 트위터봇 따라하기 - 명령어 입력시 단어를 빈칸으로 구분하여 아이디, 비번, 키워드를 인식함.

--- TIL_2021/210606_따라쓰기_트위터봇_main.py
+++ TIL_2021/210606_따라쓰기_트위터봇_main.py
@@ -2,4 +2,20 @@ import sys
 import time
 import twitter_bot_news as tb
 
-print()
+print("Process Start")
+start_time = time.time()
+_id = sys.argv[1]
+_ps = sys.argv[2]
+keyword = sys.argv[3].strip()
+
+BOT = tb.NewsBot()
+BOT.login(_id, _ps)
+hashtag = "#뉴스 #자동화 #코드"
+
+BOT.tweet_all_news(keyword, hashtag)
+time.sleep(10)
+BOT.kill()
+print("Process Done")
+end_time = time.time()
+print("The Job Took " + str(end_time - start_time) + "Second")
+
