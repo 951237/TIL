cb1fd8baaf564f7c0ca9fa967af867af74ec943e
951237 <951237@gmail.com>
2021-07-03 01:44:19 +0900

    Doc : Update - 주석달기

--- TIL_2021/210630_재귀함수 프랙탈.py
+++ TIL_2021/210630_재귀함수 프랙탈.py
@@ -16,11 +16,11 @@ def tree(size, levels, angle): # 사이즈, 깊이, 각도 설정
 
 	tree(size * 0.8, levels -1, angle)	# 크기를 80%, 깊이 -1, 회전 각도는 같게 - 재귀함수
 
-	left(angle *2)
+	left(angle *2)		# 왼쪽으로 각도 * 2 전환하기 : 이미 오른쪽으로 회전을 했기때문에 반대편으로 가기 위해서는 2배만큼 돌기
 
-	tree(size * 0.8, levels -1, angle)
+	tree(size * 0.8, levels -1, angle)	# 함수안에서 본인 함수 실행. 선분의 길이를 80%만큼, 깊이를 줄임
 
-	right(angle)
+	right(angle)	# 오른쪽으로 회전
 	backward(size)
 
 # 눈꽃 결정 프렉탈 그리기
