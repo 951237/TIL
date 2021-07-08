baf072d0c197fd2f38014c595afce56b8634e093
951237 <951237@gmail.com>
2021-07-09 01:30:56 +0900

    Doc : Create - 로지스틱회귀 - 과소적합, 과대적합에 유의하면 매개변수 사용하기 C의 값이 클수록 규제 풀어주기

--- TIL_2021/210708_머신러닝 분류용 선형모델 로지스틱 회귀.py
+++ TIL_2021/210708_머신러닝 분류용 선형모델 로지스틱 회귀.py
@@ -4,4 +4,15 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 cancer = load_breast_cancer()
-X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, )
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify = cancer.target, random_state = 1 )
+
+
+logreg = LogisticRegression().fit(X_train, y_train)
+print(f"훈련세트 점수 : {logreg.score(X_train, y_train)}")
+print(f"테스트세트 점수 : {logreg.score(X_test, y_test)}")
+
+# 제약 풀기 C 값이 높을경우, 제약강화 :  C 값이 낮을 경우
+logreg100 = LogisticRegression(C=100).fit(X_train, y_train)
+print(f"훈련세트 점수 : {logreg100.score(X_train, y_train)}")
+print(f"테스트 점수 : {logreg100.score(X_test, y_test)}")
+
