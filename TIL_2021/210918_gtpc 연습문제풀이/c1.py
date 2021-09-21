ed0b6ef78d6f67bb1578d4639f4eeb45b51d51a9
951237 <951237@gmail.com>
2021-09-21 15:22:13 +0900

    Doc : Update - 최단거리 길찾기 - 해결하지 못함. - 팩토리얼 ㄱㅖ산식으로 해결하려고 햇는데, 해결되지 않음

--- TIL_2021/210918_gtpc 연습문제풀이/c1.py
+++ TIL_2021/210918_gtpc 연습문제풀이/c1.py
@@ -1,10 +1,16 @@
 import math
 n, m = input("팩토리얼 구할 숫자 입력하세요 : ").split()
-n = int(n)
-m = int(m)
+a = int(n)
+b = int(m)
 
-print(n, m)
+# print(n, m)
 
-result = math.factorial(n+1) / (math.factorial(m-1) * math.factorial(n-1))
+# 두 수중에 큰 수가 n, 작은수는 m
+n = max(int(a), int(b)) - 1
+m = min(int(a), int(b)) - 1
+
+# print(n, m)
+
+result = math.factorial(n+m) / (math.factorial(m) * math.factorial(n))
 
 print(int(result))
