5e876ab68f63acea50c36daefd90ce5362f21bcc
951237 <951237@gmail.com>
2021-09-23 11:46:32 +0900

    Doc : Update - a0 problem solve try

--- TIL_2021/210918_gtpc 연습문제풀이/a1.py
+++ TIL_2021/210918_gtpc 연습문제풀이/a1.py
@@ -1,19 +1,15 @@
 # 곱해야하는 숫자 구하기
-n, m = input("팀수와 인원을 입력하시오. : ").split(" ")
-num_bread = int(n) * int(m)
-print(num_bread)
-cnt = num_bread // 50 + 1  # 몫구하기
+n, m = map(int, input("팀수와 인원을 입력하시오. : ").split(" "))
+k = ((n*m+9) // 10)
 
-# 경우의 수와 가격 구하기
-_min = 1000000000000000000000000000000000000000000
-a, b = 0, 0
-for i in range(cnt+1):
-    temp = 50 * (cnt - i) + 30 * (i)
-#     print(temp)
-    if temp < num_bread:
-        continue
-    if temp < _min:
-        _min = temp
-        a, b = (cnt - i), i
-price = 15000 * a + 9000 * b
-print(price)
+if k <= 3:
+    print("9000")
+elif k <= 5:
+    print("15000")
+elif k==6:
+    print("18000")
+elif k==7:
+    print("24000")
+else:
+    print("%d" %(k*3000))
+
