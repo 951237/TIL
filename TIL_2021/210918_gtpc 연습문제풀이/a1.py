e1c1f99a69f39e8d3f310b5dd23b0a78fe41c1b0
951237 <951237@gmail.com>
2021-10-01 01:12:33 +0900

    A1형 문제 해결- input문에 글자 삭제

--- TIL_2021/210918_gtpc 연습문제풀이/a1.py
+++ TIL_2021/210918_gtpc 연습문제풀이/a1.py
@@ -1,15 +1,26 @@
 # 곱해야하는 숫자 구하기
-n, m = map(int, input("팀수와 인원을 입력하시오. : ").split(" "))
-k = ((n*m+9) // 10)
+n, m = map(int, input().split())
+num_bread = n * m  # 구매해야하는 빵의 개수구하기
 
-if k <= 3:
-    print("9000")
-elif k <= 5:
-    print("15000")
-elif k==6:
-    print("18000")
-elif k==7:
-    print("24000")
+# 최소비용으로 빵 구매하기
+if num_bread < 30:
+    result = 9000
+    print(result)
+elif num_bread % 30 == 0:
+    result = num_bread // 30 * 9000
+    print(result)
+elif num_bread < 50:
+    result = 15000
+    print(result)
+elif num_bread % 50 == 0:
+    result = num_bread // 50 * 15000
+    print(result)
 else:
-    print("%d" %(k*3000))
-
+    num_bread_spare = num_bread % 50
+    _50 = num_bread // 50
+    if num_bread_spare > 30:
+        result = (_50 * 15000) + 18000
+        print(result)
+    else:
+        result = (_50 * 15000) + 9000
+        print(result)
