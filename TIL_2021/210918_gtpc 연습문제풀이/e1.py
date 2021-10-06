0a9c6d2971b51d46dd72cb0071b3423dec525c97
951237 <951237@gmail.com>
2021-10-06 23:03:59 +0900

    E1문제 풀이 - 시도2일차

--- TIL_2021/210918_gtpc 연습문제풀이/e1.py
+++ TIL_2021/210918_gtpc 연습문제풀이/e1.py
@@ -31,9 +31,9 @@ _map[1][1] = 1
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+step = 1  # 시작값
+cnt = 0  # 이동값
 while True:
-    step = 1  # 시작값
-    cnt = 0  # 이동값
     for i in range(1, r-1):
         for j in range(1, c-1):
             #             print(i,j)
@@ -43,12 +43,12 @@ while True:
                     nj = j + dy[a]
                     if _map[ni][nj] == 0:
                         _map[ni][nj] = step + 1  # 시작값을 1 → 2 → 3. . . 으로
-                        cnt += 1
 #                         print(f'좌표위치 : ({i},{j}), 좌표값 : {_map[ni][nj]}, 카운트 : {cnt}')
-                        step += 1
+                        cnt += 1
     if cnt == 0 or _map[tr][tc]:   # 움직일수 없거나 목표지점에 도착한 경우
         break
-
+    step += 1
+    
 if _map[tr][tc] == 0:
     print(-1)
 else:
