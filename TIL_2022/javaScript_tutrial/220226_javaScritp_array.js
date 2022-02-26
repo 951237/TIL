4cc51b26e02a97aeb0f09796deba7fc2697a5bce
951237 <951237@gmail.com>
2022-02-26 21:41:11 +0900

    Create - 자바스크립트 배열관련 메소드 연습추가 - pop, shift, push, unshift

--- TIL_2022/javaScript_tutrial/220226_javaScritp_array.js
+++ TIL_2022/javaScript_tutrial/220226_javaScritp_array.js
@@ -14,13 +14,30 @@ console.log(result1);
 
 // slice 함수
 let result2 = arr_1.slice(1,4);
-console.log(result2)
+console.log(result2);
 
 
 // sort 함수
-let result3 = arr_1.sort()
-console.log(result3)
+let result3 = arr_1.sort();
+console.log(result3);
 
 // reverse 함수
-let result4 = arr_1.reverse()
-console.log(result4)
+let result4 = arr_1.reverse();
+console.log(result4);
+
+// splice 함수 - 배열의 요소 대치
+arr_1.splice(2, 1, "서초", "역삼");
+console.log(arr_1);
+
+// pop 함수
+let data1 = arr_1.pop();  // arr1의 마지막 요소를 추출
+let data2 = arr_2.shift(); // arr_2의 첫번째 것을 추출
+console.log(data1, data2);
+
+// push 함수
+arr_1.push(data2)  // arr_1의 마지막에 data2를 넣기
+console.log(arr_1)
+
+// unshift 함수 
+arr_2.unshift(data1)
+console.log(arr_2)
