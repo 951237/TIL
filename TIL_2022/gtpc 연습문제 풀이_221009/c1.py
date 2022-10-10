0deebdfa57cb2421d539d7134214035c89a7f5bb
951237 <951237@gmail.com>
2022-10-10 23:27:30 +0900

    Update : c1문제 해결중

--- TIL_2022/gtpc 연습문제 풀이_221009/c1.py
+++ TIL_2022/gtpc 연습문제 풀이_221009/c1.py
@@ -11,50 +11,62 @@ f → s : 가장 작은 수 1개
 # 입력 받기
 i = int(input())
 
-lst_s = list(input().split(" "))
+lst_s = list(map(int, input().split(" ")))
 lst_f = []
 result = []
 
-# 배가 1대일경우
-if i == 1:
-    result = i
-
 # 배가 2대일 경우
-if i == 2:
+if i == 1 or i == 2:
     result = max(lst_s)
+    print(result)
 
 # 배가 3대일 경우
-if i == 3:
+elif i == 3:
     result = sum(lst_s)
+    print(result)
 
-# 재가 4대일 경우 
-if i > 3:
-    # s -> f
-    lst_s.sort()        # lst_s 리스트 정렬하기
-    to_f = lst_s[:2]    # 가장 작은수 2개 선택
-    lst_s.remove(to_f)  # 가장 작은수 2개 제거
-    lst_f.append(to_f)  # lst_s에서 가장 큰수 뽑아서 lst_f에 넣기
-    result.append(to_f[1])  # 가장 큰수는 result에 저장
+# 배가 4대일 경우 
+elif i > 3:
+    while True:
+        # s -> f
+        lst_s.sort()        # lst_s 리스트 정렬하기
+        n, m = lst_s[0], lst_s[1]    # 가장 작은수 2개 선택
+        del lst_s[:2]  # 가장 작은수 2개 제거
+        lst_f.append(n)  # lst_s에서 가장 큰수 뽑아서 lst_f에 넣기
+        lst_f.append(m)  # lst_s에서 가장 큰수 뽑아서 lst_f에 넣기
+        result.append(m)  # 가장 큰수는 result에 저장
+        # print(lst_s, lst_f, result)
 
-    # f->s
-    lst_f.sort()    # lst_f 정렬
-    to_s = lst_f[0]    # 가장 작은수 선택    
-    lst_f.remove(to_s)  # 가장 작은수 제거
-    lst_s.append(to_s)  # 가장 작은값 뽑아서 lst_s에 넣기
-    result.append(to_s).    # 가장 작은값 result에 넣기
+        if len(lst_f) == i:
+            break
+        
+        # f->s
+        lst_f.sort()    # lst_f 정렬
+        m = lst_f[0]    # 가장 작은수 선택    
+        del lst_f[0]
+        lst_s.append(m)  # 가장 작은값 뽑아서 lst_s에 넣기
+        result.append(m)    # 가장 작은값 result에 넣기
+        # print(lst_s, lst_f, result)
+        
 
-    # s -> f
-    lst_s.sort()    # lst_s 정렬
-    to_f = lst_s[-2:]   # 가장 큰수 2개 뽑기
-    lst_s.remove(to_f)  # 가장 큰 수 2개 제거
-    lst_f.append(to_f)  # 가장 큰 수 2개 
-    result.append(to_f[1])  # 큰값 result에 담기
+        # s -> f
+        lst_s.sort()    # lst_s 정렬
+        n, m = lst_s[-2], lst_s[-1]   # 가장 큰수 2개 뽑기
+        del lst_s[-2:]  # 가장 큰 수 2개 제거
+        lst_f.append(n)  # 가장 큰 수 2개 
+        lst_f.append(m)  # 가장 큰 수 2개 
+        result.append(m)  # 큰값 result에 담기
+        # print(lst_s, lst_f, result)
 
-    #f -> s
-    lst_f.sort()    # lst_f 정렬
-    to_s = lst_f[0]    # 가장 작은수 선택    
-    lst_f.remove(to_s)  # 가장 작은수 제거
-    lst_s.append(to_s)  # 가장 작은값 뽑아서 lst_s에 넣기
-    result.append(to_s).    # 가장 작은값 result에 넣기
-    
-
+        if len(lst_f) == i:
+            break
+        
+        #f -> s
+        lst_f.sort()    # lst_f 정렬
+        to_s = lst_f[0]    # 가장 작은수 선택    
+        del lst_f[0]    # 가장 작은수 제거
+        lst_s.append(to_s)  # 가장 작은값 뽑아서 lst_s에 넣기
+        result.append(to_s)    # 가장 작은값 result에 넣기
+        # print(lst_s, lst_f)
+        
+    print(sum(result))
